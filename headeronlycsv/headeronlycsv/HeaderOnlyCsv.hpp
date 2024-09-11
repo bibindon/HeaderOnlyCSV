e3fe415df53ec5d2634d7239da3ee98efe8b53c5
bibindon <bibindon0814@gmail.com>
2024-09-11 22:28:20 +0900

    .

--- headeronlycsv/headeronlycsv/HeaderOnlyCsv.hpp
+++ headeronlycsv/headeronlycsv/HeaderOnlyCsv.hpp
@@ -21,89 +21,47 @@ public:
         }
 
         std::ifstream ifs(filepath);
-        std::string buff;
         std::string buffComma;
-        int headerColSize = 0; // 先頭行の列数
-        int row = 0;
         bool doubleQuoteMode = false;
         std::vector<std::string> work;
+        std::istreambuf_iterator<char> itBegin(ifs);
+        std::istreambuf_iterator<char> itEnd;
 
-        // csvファイルは「"」記号で囲まれていればセル内で改行ができることを考慮する
-        // 以下の3つの行はどれも3列となる。
-        // a,b,c    ①
-        // a,b,"c
-        // d
-        // e"       ②
-        // a,"b",c  ③
-        // ③のように「"」で囲まれているが改行しない場合もある
-
-        // 改行まで読む
-        while (std::getline(ifs, buff))
+        for (; itBegin != itEnd; itBegin++)
         {
-            std::istringstream iss(buff);
-
-            // カンマまで読む
-            while (std::getline(iss, buffComma, ','))
+            if (*itBegin != ',' && *itBegin != '\n')
             {
-                // 先頭行だったら列数をカウントする
-                if (row == 0)
+                buffComma += *itBegin;
+                if (*itBegin == '"')
                 {
-                    headerColSize++;
-                }
-                trim(buffComma);
-                // 「"」記号がある
-                if (buffComma.find('"') != std::string::npos)
-                {
-                    // 「"」記号が先頭にある
-                    if (buffComma.at(0) == '"')
+                    if (doubleQuoteMode == false)
                     {
-                        // 「"」記号が最後にない（③のパターンではない）
-                        if (buffComma.find_last_of('"') != buffComma.size() - 1)
-                        {
-                            doubleQuoteMode = true;
-                            std::string work2 = buffComma;
-                            // ②のような「"」で囲まれ、改行を含む要素を読み取る
-                            while (std::getline(ifs, buff))
-                            {
-                                iss = std::istringstream(buff);
-                                while (std::getline(iss, buffComma, ','))
-                                {
-                                    work2 = work2 + "\n" + buffComma;
-                                    if (buffComma.find_last_of('"') == buffComma.size()-1)
-                                    {
-                                        doubleQuoteMode = false;
-                                        break;
-                                    }
-                                }
-                                if (doubleQuoteMode == false)
-                                {
-                                    buffComma = work2;
-                                    break;
-                                }
-                            }
-                        }
+                        doubleQuoteMode = true;
+                    }
+                    else
+                    {
+                        doubleQuoteMode = false;
                     }
                 }
+            }
+            else if (*itBegin == ',')
+            {
                 work.push_back(buffComma);
+                buffComma.clear();
             }
-            if (doubleQuoteMode == false)
+            else if (*itBegin == '\n')
             {
-                csvData.push_back(work);
-                work.clear();
-                row++;
-            }
-        }
-
-        // 行の最後の要素がなかった場合、空文字を追加する
-        // 以下のデータはどちらも3列あるとする。
-        // a,b,c
-        // a,b,
-        // そのため、空文字のデータを追加する
-        for (std::size_t i = 0; i < csvData.size(); ++i)
-        {
-            if (csvData.at(i).size() != headerColSize)
-            {
-                csvData.at(i).push_back("");
+                if (doubleQuoteMode == false)
+                {
+                    work.push_back(buffComma);
+                    buffComma.clear();
+                    csvData.push_back(work);
+                    work.clear();
+                }
+                else
+                {
+                    buffComma += *itBegin;
+                }
             }
         }
 
