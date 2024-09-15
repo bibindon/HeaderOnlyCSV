3a685db02dc368bec5146371b9393e7601584f64
bibindon0814 <bibindon0814@gmail.com>
2024-09-15 21:25:34 +0900

    .

--- Sample/Sample/main.cpp
+++ Sample/Sample/main.cpp
@@ -3,7 +3,40 @@
 
 int main()
 {
-    std::vector<std::vector<std::string>> csvData = csv::Read("test.csv");
-    csv::Write("test2.csv", csvData);
+    {
+        std::vector<std::vector<std::string>> csvData = csv::Read("test.csv");
+        csv::Write("test2.csv", csvData);
+    }
+
+    {
+        std::vector<std::vector<std::string>> csvData = csv::ReadFromString("a,b,c\nd,e,f\ng,h,i");
+        std::size_t s =csvData.size();
+    }
+    // おまけ
+    // シーザー暗号
+    {
+        std::string str("あいうえお");
+        const char* sz = str.c_str();
+        std::size_t _size = str.size();
+        char* work = nullptr;
+        work = new char[_size+1];
+        strcpy_s(work, _size+1, str.c_str());
+
+        // 暗号化
+        for (int i = 0; i < _size + 1; ++i)
+        {
+            work[i] = work[i]+10;
+        }
+        std::string str2(work);
+        // 複合化
+        for (int i = 0; i < _size + 1; ++i)
+        {
+            work[i] = work[i]-10;
+        }
+        std::string str3(work);
+
+        delete[] work;
+    }
+
     return 0;
 }
