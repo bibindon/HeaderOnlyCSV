78b1299adeb8a94b6b6a8c5f614093a7da4528f1
bibindon <bibindon0814@gmail.com>
2025-05-02 09:16:39 +0900

    .

--- Sample/Sample/main.cpp
+++ Sample/Sample/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include "../../HeaderOnlyCsv.hpp"
+#include <cassert>
 
 int main()
 {
     {
         std::vector<std::vector<std::string>> csvData = csv::Read("test.csv");
-        csv::Write("test2.csv", csvData);
+        csv::Write("test.out.csv", csvData);
+    }
+
+    // カンマ「,」が含まれているcsvファイル
+    {
+        std::vector<std::vector<std::string>> csvData = csv::Read("test2.csv");
+        assert(csvData.size() == 2);
+        assert(csvData.at(0).size() == 4);
+        assert(csvData.at(1).size() == 4);
+
+        assert(csvData.at(0).at(2) == "\"c,d\"");
+        assert(csvData.at(1).at(1) == "\"test,test\"");
+
+        csv::Write("test2.out.csv", csvData);
     }
 
     {
